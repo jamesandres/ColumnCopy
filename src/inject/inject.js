f8b5d6c6edeec1eda59395d0efd29ea6064cb029
James Andres <james.andres@gmail.com>
2013-04-13 13:01:52 -0400

    Proof of concept.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,69 @@
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+(function (window, $) {
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
+  /**
+   * The ColumnSelect class-y function.
+   *
+   * Given a table, ...
+   */
+  function ColumnSelect(table) {
+    this.table = table;
 
-	}
-	}, 10);
-});
+    this.init(this.$table);
+  }
+
+  ColumnSelect.prototype.init = function (table) {
+    var that = this;
+
+    $('th,td', table).click(function (e) { that.handleCellClick(e, this); });
+  };
+
+  ColumnSelect.prototype.handleCellClick = function (e, cell) {
+    // Only select the column on Alt + Click
+    if (!e.altKey) {
+      return;
+    }
+
+    var $column = this.getColumnContainingCell(cell);
+
+    if ($column) {
+      this.selectColumnAnimation($column);
+      this.copyColumnToClipboard($column);
+    }
+  };
+
+  ColumnSelect.prototype.getColumnContainingCell = function (cell) {
+    // Find the column number this cell belongs to, ie: 3rd cell is 3rd column.
+    var n = $(cell).index();
+
+    // Unknown error, cell not found in row, cell is not inside a row, or similar.
+    if (n < 0) {
+      return false;
+    }
+
+    // Get all Nth column cells in the table
+    return $('th:nth-child(' + (n + 1) + '),td:nth-child(' + (n + 1) + ')', this.table);
+  };
+
+  ColumnSelect.prototype.selectColumnAnimation = function ($column) {
+    $column.css('box-shadow', 'inset 0px 0px 5px 1px rgba(175, 211, 255, 1.0)');
+    $column.animate({ 'box-shadow-color': 'rgba(175, 211, 255, 0.0)' });
+  }
+
+  ColumnSelect.prototype.copyColumnToClipboard = function ($column) {
+    var cellHtml = [];
+
+    $column.each(function () {
+      cellHtml.push($(this).html());
+    });
+
+    console.log(cellHtml.join("\n"));
+  }
+
+
+  // Bind the column select plugin to every table on the page
+  $('table').each(function () {
+    $(this).data('_ColumnSelect', new ColumnSelect(this));
+  });
+
+
+}(window, jQuery));
