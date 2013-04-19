91cc7fa96744e2080c6eb6c7c641444fc573d56d
James Andres <james.andres@gmail.com>
2013-04-18 22:39:31 -0400

    Adding support for table cells containing HTML.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -85,7 +85,7 @@
 
         for (i = map.length - 1; i >= 0; i--) {
           if (cellMap.indexOf(map[i]) !== -1) {
-            row.push($this.html());
+            row.push(that.getCellText($this[0]));
             column.push(this);
             break;
           }
@@ -129,13 +129,60 @@
       row = [];
 
       $('td,th', this).each(function () {
-        row.push($(this).html());
+        row.push(that.getCellText(this));
       });
 
       values.push(row.join(that.settings.columnSeperator));
     });
 
     return values;
+  };
+
+  /**
+   * An similar function to jQuery.text(). This recursively digs through
+   * children of a DOM node and retrieves all text nodes and values of relevant
+   * form elements.
+   *
+   * Original concept by James Padolsey.
+   * See: http://james.padolsey.com/javascript/replacing-text-in-the-dom-its-not-that-simple/
+   */
+  ColumnSelect.prototype.getCellText = function (cell) {
+    var next, result = [];
+
+    if (cell.nodeType === 1) { // Element node
+      if (cell.nodeName === 'INPUT') {
+        switch (cell.type) {
+          case 'button':
+          case 'checkbox':
+          case 'file':
+          case 'hidden':
+          case 'image':
+          case 'password':
+          case 'radio':
+          case 'range':
+          case 'reset':
+          case 'search':
+          case 'submit':
+            // Skip these input types, note that TEXTAREA contents are a text node
+            // and will be captured by the recursion.
+            break;
+          default:
+            result.push(cell.value);
+            break;
+        }
+      }
+
+      if (cell = cell.firstChild) {
+        do {
+          next = cell.nextSibling;
+          result.push(this.getCellText(cell));
+        } while(cell = next);
+      }
+    } else if (cell.nodeType === 3) { // Text node
+      return cell.data;
+    }
+
+    return result.join('');
   };
 
   ColumnSelect.prototype.copiedToClipboardAnimation = function ($column) {
